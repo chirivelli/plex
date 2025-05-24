222170e5531b5457e1c275d70dbb3df35808cd3f
Sathwik <sathwikchirivelli@gmail.com>
2025-05-24 17:09:17 +0530

    initial commit

---
+++ src/main/java/net/sathwik/plex/PlexApplication.java
@@ -0,0 +1,13 @@
+package net.sathwik.plex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sathwik/plex/PlexApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sathwik.plex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
